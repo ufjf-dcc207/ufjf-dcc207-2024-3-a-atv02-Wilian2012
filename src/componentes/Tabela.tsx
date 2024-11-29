e41690520dc44d8b9023fb740483413f52a5d099
wilian santos <14809032647@estudante.ufjf.br>
2024-11-28 21:33:18 -0300

    configurando gh pages e executando run deploy

--- src/componentes/Tabela.tsx
+++ src/componentes/Tabela.tsx
@@ -1,12 +1,13 @@
 import Linha from './Linha';
 
+
 const data = [
-  { dia: 'Segunda-feira', hora: '08:00 - 10:00', disciplina: 'Matemática' },
-  { dia: 'Segunda-feira', hora: '10:00 - 12:00', disciplina: 'História' },
-  { dia: 'Terça-feira', hora: '08:00 - 10:00', disciplina: 'Física' },
-  { dia: 'Quarta-feira', hora: '10:00 - 12:00', disciplina: 'Química' },
-  { dia: 'Quinta-feira', hora: '08:00 - 10:00', disciplina: 'Geografia' },
-  { dia: 'Sexta-feira', hora: '10:00 - 12:00', disciplina: 'Educação Física' },
+  { dia: 'Segunda-feira', hora: '  08:00 - 10:00', disciplina: '  Matemática' },
+  { dia: 'Segunda-feira', hora: '  10:00 - 12:00', disciplina: '  História' },
+  { dia: 'Terça-feira', hora: '  08:00 - 10:00', disciplina: '  Física' },
+  { dia: 'Quarta-feira', hora: '  10:00 - 12:00', disciplina: '  Química' },
+  { dia: 'Quinta-feira', hora: '  08:00 - 10:00', disciplina: '  Geografia' },
+  { dia: 'Sexta-feira', hora: '  10:00 - 12:00', disciplina: '  Educação Física' },
 ];
 
 function Tabela()
